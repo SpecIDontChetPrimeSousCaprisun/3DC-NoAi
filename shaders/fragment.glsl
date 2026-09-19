390181601bbe09d84cf4faec2349246e657189a3
SpecIDontChetPrimeSousCaprisun <corieux1@gmail.com>
2026-09-19 13:38:43 +0200

    Fixed point lights*

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -60,9 +60,9 @@ vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
     vec3 reflectDir = reflect(-lightDir, normal);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     // attenuation
-    float distance    = length(light.position - fragPos);
-    float attenuation = 1.0 / (light.constant + light.linear * distance + 
-  			     light.quadratic * (distance * distance));    
+    float distanceLF  = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distanceLF + 
+  			     light.quadratic * (distanceLF * distanceLF));    
     // combine results
     vec3 ambient  = light.ambient  * vec3(texture(material.diffuse, TexCoords));
     vec3 diffuse  = light.diffuse  * diff * vec3(texture(material.diffuse, TexCoords));
@@ -78,7 +78,7 @@ void main() {
     vec3 viewDir = normalize(viewPos - FragPos);
 
     vec3 result = CalcDirLight(dirLight, norm, viewDir);
-    //for (int i = 0; i < NR_POINT_LIGHTS; i++) result += CalcPointLight(pointLights[i], norm, FragPos, viewDir);
+    for (int i = 0; i < NR_POINT_LIGHTS; i++) result += CalcPointLight(pointLights[i], norm, FragPos, viewDir);
 
     FragColor = vec4(result, 1.0);
 } 
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -87,16 +87,16 @@ void Shader::setDirLight(DirLight light) {
 
 void Shader::setPointLight(int number, PointLight light) {
     std::ostringstream ss;
-    ss << "pointLights[" << number;
+    ss << "pointLights[" << number << "]";
     std::string name = ss.str();
 
-    setVec3((name + ".position]").c_str(), light.position); 
-    setFloat((name + ".constant]").c_str(), light.constant);
-    setFloat((name + ".linear]").c_str(), light.linear);
-    setFloat((name + ".quadratic]").c_str(), light.quadratic);
-    setVec3((name + ".ambient]").c_str(), light.ambient);
-    setVec3((name + ".diffuse]").c_str(), light.diffuse);
-    setVec3((name + ".specular]").c_str(), light.specular);
+    setVec3((name + ".position").c_str(), light.position); 
+    setFloat((name + ".constant").c_str(), light.constant);
+    setFloat((name + ".linear").c_str(), light.linear);
+    setFloat((name + ".quadratic").c_str(), light.quadratic);
+    setVec3((name + ".ambient").c_str(), light.ambient);
+    setVec3((name + ".diffuse").c_str(), light.diffuse);
+    setVec3((name + ".specular").c_str(), light.specular);
 }
 
 void Shader::setMaterial(Material material) {
